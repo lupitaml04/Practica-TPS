e8643c49eb91d3e62943823a78187359823e9855
lupitaml04 <lupita_ml04@hotmail.com>
2013-08-28 17:48:21 -0500

    P1 28/08/13

--- Practica1/src/Practica1.java
+++ Practica1/src/Practica1.java
@@ -8,10 +8,28 @@
  */
 import java.util.*;
 import java.io.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 class Linea{
 	String etiqueta, codigo, operando;
+	int lin;
+	
+	public Linea(String eti,int l)
+	{
+		etiqueta=eti;
+		lin=l;
+	}
 	
 	public void validarEtiqueta(){
+		
+		Pattern pat = Pattern.compile("^[a-zA-Z][a-zA-Z_0-9]{0,8}");
+        Matcher mat = pat.matcher(etiqueta);
+        if (mat.matches()) {
+        	System.out.println("SI "+lin);
+        } else {
+        	System.out.println("NO "+lin);
+          }
 	}
 	
 	public void validarCodigo(){
@@ -23,33 +41,71 @@ class Linea{
 
 public class Practica1 {
 	Scanner Leer=new Scanner(System.in);
-	String ruta;
-	String texto;
+	String ruta, texto, archivo;
+	int linea =-1;
 	
-	public void leerArchivo(){
-		//System.out.println("Escribe la ruta del arhivo");
-		//ruta=Leer.next();
+	public Practica1(String r){
+		ruta=r;
+	}
+	
+	public void validarRuta(){
+		File fichero = new File(ruta);
+        if (fichero.exists())
+        {
+        	archivo=fichero.getName();
+        	System.out.println(archivo);
+        	 leerArchivo();
+        }
+        	
+           
+        else
+        	System.out.println("La ruta no existe");
+     }
+	
+	
+	public void leerArchivo() {
+		
+		
 		try{
-		RandomAccessFile archi=new RandomAccessFile("archivo.ASM","r");
-		while(archi.getFilePointer()!=archi.length())
+			RandomAccessFile archi=new RandomAccessFile(new File(ruta),"r");
+			while(archi.getFilePointer()!=archi.length())
 		{
-			texto+=archi.readUTF();
+			texto=archi.readLine();
+			linea++;
+			revisarLinea(texto);
+			System.out.println(texto);
 		}
+		
 		archi.close();
-		System.out.println(texto);
-	}
-	catch(IOException ioe)
+		}
+	catch(IOException e)
 	{
-		System.out.println("Error al abrir el archivo");
-	}
-
+		System.out.println("Error");
 	}
 		
+	}
+	
+    public void revisarLinea(String tex){
+     Linea lin= new Linea(texto,linea);
+     lin.validarEtiqueta();
+     
+         	
+    }
     
-    public static void main(String[] args) {
+    public static void main(String[] args){
     	
     	
-    	Practica1 obj= new Practica1();
-    	obj.leerArchivo();
+    	
+    	Scanner Leer=new Scanner(System.in);
+    	
+    	String ruta="hola.txt";
+    	
+    	System.out.println("¿Cual es la ruta del archivo?");
+    	ruta=Leer.next();
+    	
+    	Practica1 obj= new Practica1(ruta);
+    	obj.validarRuta();
+    	
+    	
     }
 }
