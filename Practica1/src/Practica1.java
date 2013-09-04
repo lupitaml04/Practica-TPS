6db6b223291d39abcad4d50c9a2b7191c2017690
lupitaml04 <lupita_ml04@hotmail.com>
2013-09-03 23:39:47 -0500

    Practica 1.4

--- Practica1/src/Practica1.java
+++ Practica1/src/Practica1.java
@@ -6,98 +6,25 @@
  * @author 
  * @version 1.00 2013/8/24
  */
+
 import java.util.*;
 import java.io.*;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
-
-class Linea{
-	String etiqueta, codigo, operando;
-	int lin;
-	
-	public void  validarEtiqueta(String eti,int l){
-		etiqueta=eti;
-		lin=l;
-		Pattern pat = Pattern.compile("^[a-zA-Z][a-zA-Z_0-9]{0,8}");
-        Matcher mat = pat.matcher(etiqueta);
-//        Practica1 p=new Practica1();
-        if (mat.matches()) {
-        	System.out.println("eti SI "+lin);
-        	//p.escribirInstruccion(etiqueta);
-        } else {
-        	System.out.println("eti NO "+lin);
-        	//p.escribirError("la etiqueta no es valida\n "+lin);
-          }
-	}
-	
-	public void validarCodigo(String cod,int l){
-		codigo=cod;
-		lin=l;
-		Pattern pat = Pattern.compile("^[a-zA-Z][a-zA-Z_0-9]{0,8}");
-        Matcher mat = pat.matcher(codigo);
-               if (mat.matches()) {
-        	System.out.println("codigo SI "+lin);
-        	
-        } else {
-        	System.out.println("codigo NO "+lin);
-        
-          }
-		}
-		
-	public void validarOperando(String oper,int l){
-		operando=oper;
-		lin=l;
-		Pattern pat = Pattern.compile("^[a-zA-Z][a-zA-Z_0-9]{0,8}");
-        Matcher mat = pat.matcher(operando);
-               if (mat.matches()) {
-        	System.out.println("oper SI "+lin);
-        	
-        } else {
-        	System.out.println("oper NO "+lin);
-        
-          }
-		}
-}
 
 public class Practica1 {
 	Scanner Leer=new Scanner(System.in);
-	String ruta, texto,eti,codop,oper, archivo,archivoInst,archivoErr;
+	String ruta, texto, archivo,archivoInst,archivoErr;
 	int linea =-1;
 	boolean end;
 	
 	public Practica1(String r){
 		ruta=r;
 	}
-	public void crearArchivos(){
-		
-		File fichero = new File(ruta);
-		archivoInst=fichero.getName().substring(0,fichero.getName().indexOf('.'))+".INST";
-		archivoErr=fichero.getName().substring(0,fichero.getName().indexOf('.'))+".ERR";
-		
-		try{
-			RandomAccessFile archinst=new RandomAccessFile(archivoInst,"rw");
-			archinst.writeUTF(" LINEA\t\tETQ\t\tOPER\n");
-			archinst.writeUTF("\n...........................................................\n");	
-			archinst.close();
-		}
-	      catch(IOException e){
-		    System.out.println("Error");
-	       }
-	       
-	    try{
-			RandomAccessFile archierr=new RandomAccessFile(archivoErr,"rw");
-			archierr.writeUTF("ERRORES\n.........................................");	
-			archierr.close();
-		}
-	      catch(IOException e){
-		    System.out.println("Error");
-	       }
+	
+	public Practica1(){
 	}
 	
 	public void validarRuta(){
-		File fichero = new File(ruta);
-		
+		File fichero = new File(ruta);		
         if (fichero.exists())
         {
         	if(fichero.getName().endsWith(".ASM"))
@@ -112,37 +39,76 @@ public class Practica1 {
           else
         	System.out.println("La ruta no existe");
      }
-	
+    
+	public void crearArchivos(){		
+		File fichero = new File(ruta);
+		archivoInst=fichero.getName().substring(0,fichero.getName().indexOf('.'))+".INST";
+		archivoErr=fichero.getName().substring(0,fichero.getName().indexOf('.'))+".ERR";
+		File inst = new File(archivoInst);
+		File err = new File(archivoErr);
+		 if(inst.exists())
+		 	inst.delete();
+		 if(err.exists())
+		 	err.delete();		
+		try{
+			RandomAccessFile archinst=new RandomAccessFile(archivoInst,"rw");
+			archinst.writeUTF("	LINEA	ETQ	CODOP	OPER");
+			archinst.writeUTF("\r\n...........................................................\r\n");	
+			archinst.close();
+		}
+	      catch(IOException e){
+		    System.out.println("Error");
+	       }	       
+	    try{
+			RandomAccessFile archierr=new RandomAccessFile(archivoErr,"rw");
+			 archierr.writeUTF("	ERRORES\r\n.........................................\r\n");	
+			archierr.close();
+		}
+	      catch(IOException e2){
+		    System.out.println("Error");
+	       }
+	}
 	
 	public void leerArchivo() {		
 		try{
 			RandomAccessFile archi=new RandomAccessFile(new File(ruta),"r");
-			while(archi.getFilePointer()!=archi.length()){
+			while(archi.getFilePointer()!=archi.length() && !end){
 			texto=archi.readLine();
 			linea++;
 			revisarLinea();
-			System.out.println(texto);
 		    }
-		
+		    if(!end)
+		    	escribirError("No hay END",archivoErr);
 		    archi.close();
 		    }
 	        catch(IOException e){
 		      System.out.println("Error");
 	        }		
 	}
-	
-	
+		
     public void revisarLinea(){
-    	Linea lin= new Linea();
-        int cont=0,tam,edo=0;
-        char[] cad;
-        cad = texto.toCharArray();
+    	Linea lin= new Linea(linea,archivoErr);     		   	
+    	String eti,codop,oper;
+        int cont, edo, tam;
+        char[] cad = texto.toCharArray();
+        boolean etiq, cod, op;
+        
+        eti=null; 
+        codop=null; 
+        oper=null;
+        cont=0; 
+        edo=0; 
         tam=texto.length();
-        System.out.println("tamaño "+tam);
-        while(edo != 10){
+        etiq=false;
+        cod=false;
+        op=false;
+        
+         while(edo != 10){
         	switch(edo){
         		case 0:
-        			System.out.println("edo "+edo);
+        			if(cont==tam){
+        				edo=10;
+        			}else
         			if(cad[cont]==';'){
         				edo=1;
     				    cont++;
@@ -150,59 +116,63 @@ public class Practica1 {
     				else
     					if(cad[cont]==' ' || cad[cont]=='	'){
     						edo=4;
-    						eti=cad[cont]+"";
+    						eti="NULL";
+    						etiq=true;
     						cont++;
     					}
-    					else{
-    						cont++;
+    					else{	
     						eti=cad[cont]+"";
+    						cont++;
     						edo=3;
     					}		
     		     break;
-    		     
     		     case 1:
-    		     	System.out.println("edo "+edo);
     		     	if(tam==cont)
     		     		edo=10;
     		     		else{
     		     			cont++;
     					    edo=2;
-    				    }
-    				
+    				    }  				
      	         break;
-     	         
      	         case 2:
-     	         	System.out.println("edo "+edo);
     		     	if(tam==cont)
     		     		edo=10;
     		     		else{
-    						 cont++;
+    						cont++;
     					    edo=2;
     				    }
      	         break;
      	         case 3:
-     	         	System.out.println("edo "+edo+" cont "+cont+"tam "+tam);
-     	         	
      	         	if(cont==tam){
-     	         		escribirError("No hay Codigo de operacion en la linea " +linea);
+     	         		escribirError(linea +" No hay Codigo de operacion \r\n",archivoErr);
      	         		edo=10;
      	         	}
      	         	else
+     	         		if(cad[cont]==';'){
+     	         			escribirError("Linea "+ linea+ "No hay codigo de operacion",archivoErr);
+     	         			edo=1;
+     	         			cont++;
+     	         		}
+     	         		else
      	         	if(cad[cont]==' ' || cad[cont]=='	'){
-     	         		lin.validarEtiqueta(eti,linea);
+     	         		etiq=lin.validarEtiqueta(eti);
      	         		cont++;
      	         		edo=5;
      	         	}
      	         	else{
-     	         		eti.concat(cad[cont]+"");
-     	         		cont++;
-     	         		edo=3;
+     	         		eti+=cad[cont];
+     	         		        cont++;
+     	         		        edo=3;
      	         	}		
      	         break;
      	         case 4:
-     	         	System.out.println("edo "+edo);
      	         	if(tam==cont){
      	         		edo=10;
+     	         	}
+     	         	else
+     	         	if(cad[cont]==';'){
+     	         	edo=1;
+     	         	cont++;
      	         	}
      	         	else
      	         		if(cad[cont]==' ' || cad[cont]=='	'){
@@ -210,48 +180,74 @@ public class Practica1 {
      	         			edo=4;
      	         		}
      	         		else{
+     	         			if((cad[cont]=='E'||cad[cont]=='e')&& (cont+3)<=tam){
+     	         				String subStr=texto.substring(cont, cont+3);
+                                if(subStr.toUpperCase().equals("END")){
+                                	end=true;
+                                	edo=10;
+                                }   	
+     	         			}
+     	         			if(!end){
      	         			codop=cad[cont]+"";
      	         			cont++;
      	         			edo=6;
+     	         			}	
      	         		}   	         			
      	         break;
      	         case 5:
-     	         	System.out.println("edo "+edo);
      	         	if(tam==cont){
-     	         		escribirError("No hay codigo de operacion "+ linea);
-     	         		edo=10;
+     	         		escribirError("Linea "+ linea+" No hay codigo de operacion ",archivoErr);
+     	                edo=10;
      	         	}
-     	         	else	
-     	         	if(cad[cont]==' '||cad[cont]=='	'){
+     	         	else
+     	         		if(cad[cont]==';'){
+     	         			escribirError("Linea "+ linea+" No hay codigo de operacion ",archivoErr);
+     	                    edo=1;
+     	                    cont++;	
+     	         		}
+     	         		else	
+     	         	if(cad[cont]==' '|| cad[cont]=='	'){
      	         		cont++;
      	         		edo=5;
      	         	}
      	         	else{
-     	         		cont++;
-     	         		codop.concat(cad[cont]+"");
+     	         		codop=cad[cont]+"";
      	         		edo=6;
+     	         		cont++;
      	         	}		
      	         break;
      	         case 6:
-     	         	System.out.println("edo "+edo);
      	         	if(tam==cont){
-     	         		lin.validarCodigo(codop,linea);
+     	         		cod=lin.validarCodigo(codop);
      	         		edo=10;
+     	         		op=true;
+     	         		oper="NULL";
      	         	}
      	         	else
+     	         	if(cad[cont]==';'){
+     	         		edo=1;
+     	         		cod=lin.validarCodigo(codop);
+     	         		op=true;
+     	         		oper="NULL";
+     	         	}	
+     	         	else
      	         	if(cad[cont]==' '|| cad[cont]=='	'){
-     	         		lin.validarCodigo(codop,linea);
+     	         		cod=lin.validarCodigo(codop);
      	         		cont++;
      	         		edo=7;
      	         	}
      	         	else{
-     	         		codop.concat(cad[cont]+"");
+     	         		codop=codop+cad[cont];
      	         		edo=6;
-     	         			cont++;
+     	         		cont++;
      	         	}
      	         break;
      	         case 7:
-     	         	System.out.println("edo "+edo);
+     	         	if(tam==cont){
+     	         		oper="NULL";
+     	         		op=true;
+     	         	}
+     	         	else
      	         	if(cad[cont]==' '|| cad[cont]=='	'){
      	         		cont++;
      	         		edo=7;
@@ -260,36 +256,45 @@ public class Practica1 {
      	         		if(cad[cont]==';'){
      	         			cont++;
      	         			edo=1;
+     	         			op=true;
+     	         			oper="NULL";
      	         		}
      	         	else{
-     	         		cont++;
+     	         		
      	         		oper=cad[cont]+"";
+     	         		cont++;
      	         		edo=8;
      	         	}    	         	
      	         break;
      	         case 8:
-     	         	System.out.println("edo "+edo);
      	         	if(tam==cont){
+     	         		op=lin.validarOperando(oper);
      	         		edo=10;
      	         	}
-     	         	else	
+     	         	else
+     	         		if(cad[cont]==';'){
+     	         			op=lin.validarOperando(oper);
+     	         			edo=1;
+     	         			cont++;
+     	         		}
+     	         		else	
      	         	if(cad[cont]==' ' || cad[cont]=='	'){
-     	         		lin.validarOperando(oper,linea);
+     	         		op=lin.validarOperando(oper);
      	         		cont++;
      	         		edo=9;
      	         	}
      	         	else
      	         	if(cad[cont]==';'){
+     	         		op=lin.validarOperando(oper);
      	         		cont++;
      	         		edo=1;
      	         	}
      	         	else{
-     	         		oper.concat(cad[cont]+"");
+     	         		oper+=cad[cont];
      	         		cont++;
      	         	}		
      	         break;
      	         case 9:
-     	         	System.out.println("edo "+edo);
      	         	if(tam==cont){
      	         		edo=10;
      	         	}
@@ -304,10 +309,14 @@ public class Practica1 {
      	         			}
      	         break;       
      }	
+    }
+    if((etiq && cod) && op){
+       escribirInstruccion("	"+ linea+  "	"+ eti+"	"+codop+"	"+oper+"\r\n");
     }      	
     }
     
-    public void escribirError(String error){
+    public void escribirError(String error, String archi){
+    	archivoErr=archi;
     	try{
 			RandomAccessFile archierr=new RandomAccessFile(archivoErr,"rw");
 			archierr.seek(archierr.length());
@@ -317,10 +326,12 @@ public class Practica1 {
 	      catch(IOException e){
 		    System.out.println("Error");
 	       }
-    }   
+    }  
+    	 
     public void escribirInstruccion(String instruccion){
     	try{
     		RandomAccessFile archinst=new RandomAccessFile(archivoInst,"rw");
+    		archinst.seek(archinst.length());
 			archinst.writeUTF(instruccion);	
 			archinst.close();
 		}
@@ -333,7 +344,7 @@ public class Practica1 {
     	Scanner Leer=new Scanner(System.in);
     	String ruta;
     	System.out.println("¿Cual es la ruta del archivo?");
-    	ruta=Leer.next();
+    	ruta=Leer.nextLine();
     	Practica1 obj= new Practica1(ruta);
     	obj.validarRuta();	
     }
